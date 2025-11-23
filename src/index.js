870780056583f150dd8f069f3c4ede8ae413a17a
unknown <vguimaray@gmail.com>
2025-11-23 10:21:56 -0500

    Backend inicial completo

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { sequelize } from "./database.js";
+
+import cartRoutes from "./routes/cart.routes.js";
+import orderRoutes from "./routes/order.routes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
+
+const PORT = process.env.PORT || 3001;
+
+async function start() {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log("Base de datos conectada");
+
+    app.listen(PORT, () =>
+      console.log(`Backend escuchando en puerto ${PORT}`)
+    );
+  } catch (err) {
+    console.error("Error al iniciar:", err);
+  }
+}
+
+start();
